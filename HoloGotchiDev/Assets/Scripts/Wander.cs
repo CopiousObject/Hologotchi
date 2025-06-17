670a41348d2817316111a43747f33d3314c1f5dc
Coruscat <j0nnyawes0me45@gmail.com>
2025-06-17 16:31:53 -0400

    Added Nav AI package and implemented basic wander into Web sample scene

---
+++ HoloGotchiDev/Assets/Scripts/Wander.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Wander : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float wanderRadius;
+    private float timer = 0;
+    private float wanderTimer = 3;
+    private NavMeshAgent agent;
+    
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        timer = wanderTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= wanderTimer)
+        {
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            agent.SetDestination(newPos);
+            timer = 0;
+        }
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
+}
