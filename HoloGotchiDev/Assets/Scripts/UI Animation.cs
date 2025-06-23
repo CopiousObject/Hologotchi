0a3e37a476e97a349cc3aa53066c6907e12df12c
Noah Dailey <nwilliamd2005@gmail.com>
2025-06-23 15:34:05 -0400

    UI Functionality Pt. 3
    I tried to use the Coroutine's to make the animation smoother and it didn't appear to make much of a difference. Maybe someone else can take a look at it and make more sense of it.

--- HoloGotchiDev/Assets/Scripts/UI Animation.cs
+++ HoloGotchiDev/Assets/Scripts/UI Animation.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIAnimation : MonoBehaviour
 {
     float time = 0.0f;
+    private RectTransform rectTransform;
+
+    //private Coroutine currentAnimation;
 
     // Placements
     private Vector3 playScreen = new Vector3(73, -17, 0);
@@ -12,6 +16,11 @@ public class UIAnimation : MonoBehaviour
     private Vector3 settingsScreen = new Vector3(73, -2635, 0);
     private Vector3 notifScreen = new Vector3(1975, -2635, 0);
     private Vector3 audioScreen = new Vector3(-1920, -2635, 0);
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
 
     /// <summary>
     /// Navigates the UI to the Main Play Screen
@@ -21,7 +30,7 @@ public class UIAnimation : MonoBehaviour
         while(time <= 1.0f)
         {
             time += Time.deltaTime;
-            this.GetComponent<RectTransform>().anchoredPosition = Vector3.Lerp(this.GetComponent<RectTransform>().anchoredPosition, playScreen, time);
+            rectTransform.anchoredPosition = Vector3.Lerp(rectTransform.anchoredPosition, playScreen, EaseOut(time));
         }
         time = 0.0f;
     }
@@ -31,10 +40,15 @@ public class UIAnimation : MonoBehaviour
     /// </summary>
     public void NavigateToStats()
     {
+        //if (currentAnimation != null)
+        //    StopCoroutine(currentAnimation);
+
+        //currentAnimation = StartCoroutine(AnimateToPosition(statsScreen, 1.0f));
+
         while (time <= 1.0f)
         {
             time += Time.deltaTime;
-            this.GetComponent<RectTransform>().anchoredPosition = Vector3.Lerp(this.GetComponent<RectTransform>().anchoredPosition, statsScreen, time);
+            rectTransform.anchoredPosition = Vector3.Lerp(rectTransform.anchoredPosition, statsScreen, EaseOut(time));
         }
         time = 0.0f;
     }
@@ -47,7 +61,7 @@ public class UIAnimation : MonoBehaviour
         while (time <= 1.0f)
         {
             time += Time.deltaTime;
-            this.GetComponent<RectTransform>().anchoredPosition = Vector3.Lerp(this.GetComponent<RectTransform>().anchoredPosition, settingsScreen, time);
+            rectTransform.anchoredPosition = Vector3.Lerp(rectTransform.anchoredPosition, settingsScreen, EaseOut(time));
         }
         time = 0.0f;
     }
@@ -60,7 +74,7 @@ public class UIAnimation : MonoBehaviour
         while (time <= 1.0f)
         {
             time += Time.deltaTime;
-            this.GetComponent<RectTransform>().anchoredPosition = Vector3.Lerp(this.GetComponent<RectTransform>().anchoredPosition, notifScreen, time);
+            rectTransform.anchoredPosition = Vector3.Lerp(rectTransform.anchoredPosition, notifScreen, EaseOut(time));
         }
         time = 0.0f;
     }
@@ -73,8 +87,30 @@ public class UIAnimation : MonoBehaviour
         while (time <= 1.0f)
         {
             time += Time.deltaTime;
-            this.GetComponent<RectTransform>().anchoredPosition = Vector3.Lerp(this.GetComponent<RectTransform>().anchoredPosition, audioScreen, time);
+            rectTransform.anchoredPosition = Vector3.Lerp(rectTransform.anchoredPosition, audioScreen, EaseOut(time));
         }
         time = 0.0f;
+    }
+
+    //private IEnumerator AnimateToPosition(Vector3 target, float duration)
+    //{
+    //    Vector3 start = rectTransform.anchoredPosition;
+    //    float time = 0f;
+
+    //    while (time < duration)
+    //    {
+    //        time += Time.deltaTime;
+    //        float t = Mathf.Clamp01(time / duration);
+    //        float easedT = EaseOut(t);
+    //        rectTransform.anchoredPosition = Vector3.LerpUnclamped(start, target, easedT);
+    //        yield return null;
+    //    }
+
+    //    rectTransform.anchoredPosition = target;
+    //}
+
+    private float EaseOut(float x)
+    {
+        return 1f - Mathf.Pow(1f - x, 4f);
     }
 }
