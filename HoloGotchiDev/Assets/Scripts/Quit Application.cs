3474be3ad34a476e670bf1baa8eeefad3769a968
Noah Dailey <nwilliamd2005@gmail.com>
2025-06-24 12:49:56 -0400

    Additional UI Functions
    The sliders previously added are now colored correctly and start at the max value. Additionally the quit button now exits the application when in the test build.

---
+++ HoloGotchiDev/Assets/Scripts/Quit Application.cs
@@ -0,0 +1,15 @@
+using LookingGlass;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApplication : MonoBehaviour
+{
+    /// <summary>
+    /// Quit the application when a button is pressed
+    /// </summary>
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
