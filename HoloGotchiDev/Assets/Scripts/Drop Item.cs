d393900df80b0316b7b4bb68648f8f6b4e23798d
Noah Dailey <nwilliamd2005@gmail.com>
2025-06-25 12:59:04 -0400

    Falling Items
    Player is no able to click on any of the 5 items to drop one from the UI screen. This will eventually be connected to the Looking Glass where a modeled version of the item will drop in for the HoloPal to use. A text build with this addition is added to the repo to demonstrate this change.

---
+++ HoloGotchiDev/Assets/Scripts/Drop Item.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class DropItem : MonoBehaviour
+{
+    [SerializeField] GameObject objectToDuplicate;
+    GameObject duplicate;
+
+    /// <summary>
+    /// Will run to delete the duplicated object after falling below a certain point
+    /// </summary>
+    private void Update()
+    {
+        if (duplicate != null && duplicate.GetComponent<RectTransform>().anchoredPosition.y <= -4000)
+        {
+            Destroy(duplicate);
+        }
+    }
+
+    /// <summary>
+    /// Duplicates this GameObject and then drops it from the scene
+    /// </summary>
+    public void Drop()
+    {
+        RectTransform environment = GameObject.Find("Environment").GetComponent<RectTransform>();
+
+        duplicate = Instantiate(objectToDuplicate, environment);
+
+        duplicate.transform.SetSiblingIndex(1);
+
+        if (duplicate.GetComponent<Rigidbody2D>() == null)
+        {
+            Rigidbody2D rb = duplicate.AddComponent<Rigidbody2D>();
+            rb.gravityScale = 20f;
+        }     
+    }
+}
