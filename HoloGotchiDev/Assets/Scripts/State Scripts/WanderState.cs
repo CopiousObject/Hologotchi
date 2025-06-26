c1a184cccd007077ff6d9f363bbcd078a5718c5d
Coruscat <j0nnyawes0me45@gmail.com>
2025-06-26 12:27:29 -0400

    FSM and State Classes

---
+++ HoloGotchiDev/Assets/Scripts/State Scripts/WanderState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderState : IState
+{
+    public void UpdateState(HoloPal holopal)
+    {
+
+    }
+
+    public void OnEnter()
+    {
+
+    }
+
+    public void OnExit()
+    {
+
+    }
+}
