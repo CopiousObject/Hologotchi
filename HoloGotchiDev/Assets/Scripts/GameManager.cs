2fa226a33a4d01b6404a7027b649e0396359c738
Schmutz02 <nathan.caron29@gmail.com>
2025-06-30 15:14:40 -0400

    split spawnedObjects into distinct lists for the searching

--- HoloGotchiDev/Assets/Scripts/GameManager.cs
+++ HoloGotchiDev/Assets/Scripts/GameManager.cs
@@ -10,9 +10,17 @@ public class GameManager : MonoBehaviour
     [SerializeField] InterProcessCommunicator receiver;
     public GameObject foodPrefab;
     public GameObject waterPrefab;
-    public List<GameObject> spawnedObjects;
+    public int max_food_objects;
+    public List<GameObject> food_objects;
+    public int max_water_objects;
+    public List<GameObject> water_objects;
     private GameObject currentObject;
 
+    void Awake()
+    {
+        food_objects = new List<GameObject>(max_food_objects);
+        water_objects = new List<GameObject>(max_water_objects);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -23,25 +31,31 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        if (spawnedObjects.Count > objCount)
-        {
-            Destroy(spawnedObjects[0]);
-            spawnedObjects.Remove(spawnedObjects[0]);
-        }
-
         if (Input.GetKeyUp(KeyCode.S))
         {
-            SpawnItem(foodPrefab);
+            SpawnItem(foodPrefab, food_objects);
         }
         if (Input.GetKeyUp(KeyCode.D))
         {
-            SpawnItem(waterPrefab);
+            SpawnItem(waterPrefab, water_objects);
         }
     }
 
-    public void SpawnItem(GameObject current)
+    public void SpawnItem(GameObject current, List<GameObject> object_list)
     {
-        spawnedObjects.Add(Instantiate(current, this.gameObject.transform.position, Quaternion.identity));
+        if (object_list.Count == object_list.Capacity)
+        {
+            var oldest_object = object_list[0];
+
+            oldest_object.transform.SetPositionAndRotation(gameObject.transform.position, Quaternion.identity);
+
+            object_list.RemoveAt(0);
+            object_list.Add(oldest_object);
+            
+            return;
+        }
+
+        object_list.Add(Instantiate(current, gameObject.transform.position, Quaternion.identity));
     }
 
     /// <summary>
@@ -50,6 +64,6 @@ public class GameManager : MonoBehaviour
     /// <param name="message"></param>
     public void ReceiveMessage(string message)
     {
-        if(message == "Drop Item") SpawnItem(foodPrefab);
+        if(message == "Drop Item") SpawnItem(foodPrefab, food_objects);
     }
 }
