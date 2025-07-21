670799f31e3a2dd80daca029d19655520917ec99
Noah Dailey <nwilliamd2005@gmail.com>
2025-07-21 15:49:35 -0400

    File Saving/Loading
    It works now. The file of the saved data is in a bit of a weird spot as it is per user not, per build, which is expected. If anyone needs to be pointed in the direction to the file, please ask and I can help point in the right direction. there is still one small issue as if values of the stats are 0, they get reset to 1 upon loading but it should be a quick fix.

--- HoloGotchiDev/Assets/Scripts/File Manager.cs
+++ HoloGotchiDev/Assets/Scripts/File Manager.cs
@@ -38,6 +38,14 @@ public class FileManager : MonoBehaviour
         {
             Load();
         }
+        else
+        {
+            statBars[0].value = 1f;
+            statBars[1].value = 1f;
+            statBars[2].value = 1f;
+            statBars[3].value = 1f;
+            statBars[4].value = 1f;
+        }
     }
 
     /// <summary>
@@ -47,7 +55,7 @@ public class FileManager : MonoBehaviour
     {
         time += Time.deltaTime;
 
-        Debug.Log("THIS IS TIME FOR SAVING: " + time);
+        //Debug.Log("THIS IS TIME FOR SAVING: " + time);
         if (time >= 30.0f)
         {
             Debug.Log("Saving");
@@ -134,18 +142,18 @@ public class FileManager : MonoBehaviour
     /// </summary>
     private void AssignValues()
     {
-        // Stat values
+        // Assign the current values
         statBars[0].value = gameData.waterValue;
         statBars[1].value = gameData.hungerValue;
         statBars[2].value = gameData.playValue;
         statBars[3].value = gameData.chatValue;
         statBars[4].value = gameData.kemptValue;
+        tempGrowthStage = gameData.growthStage;
+        tempTime = gameData.growthTime;
 
         // Send a message through ipc to Holopal
         communicator.SendData("{0}", gameData.growthStage);
         communicator.SendData("Time:" + gameData.growthTime);
-        tempGrowthStage = gameData.growthStage;
-        tempTime = gameData.growthTime;
     }
 
     /// <summary>
@@ -154,7 +162,7 @@ public class FileManager : MonoBehaviour
     /// <param name="message"></param>
     public void ReceiveMessage(string message)
     {
-        Debug.Log("Received IPC Message: " + message);
+        //Debug.Log("Received IPC Message: " + message);
         if (message == "0" || message == "1" ||
             message == "2" || message == "3") 
             int.TryParse(message, out tempGrowthStage);
