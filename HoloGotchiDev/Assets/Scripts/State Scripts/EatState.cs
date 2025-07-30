f3339f70fb628731d8ff5846b86ec4b7321a5a8f
Schmutz02 <nathan.caron29@gmail.com>
2025-07-30 13:25:29 -0400

    eating animation first pass

--- HoloGotchiDev/Assets/Scripts/State Scripts/EatState.cs
+++ HoloGotchiDev/Assets/Scripts/State Scripts/EatState.cs
@@ -6,6 +6,8 @@ public class EatState : IState
 {
     GameObject food_target;
 
+    bool eating;
+
     public EatState(GameObject food_target)
     {
         this.food_target = food_target;
@@ -13,23 +15,24 @@ public class EatState : IState
 
     public void UpdateState(HoloPal holopal)
     {
-        if (!food_target || holopal.food >= 0.8f)
+        if (!eating)
         {
-            holopal.ChangeState(null);
-            return;
+            holopal.Nav_Agent.SetDestination(food_target.transform.position);
         }
-
-        holopal.Nav_Agent.SetDestination(food_target.transform.position);
     }
 
     public void OnEnter(HoloPal holopal)
     {
-
+        eating = false;
     }
 
     public void OnExit(HoloPal holopal)
     {
-
+        eating = false;
+        holopal.food = 1f;
+        holopal.Nav_Agent.isStopped = false;
+        holopal.Spawner.FoodObjects.Remove(food_target);
+        Object.Destroy(food_target);
     }
 
     public void OnTriggerEnter(HoloPal holopal, Collider other)
@@ -38,9 +41,14 @@ public class EatState : IState
         {
             if (holopal.food < 0.8f)
             {
-                holopal.food = 1f;
-                holopal.Spawner.FoodObjects.Remove(food_target);
-                Object.Destroy(food_target);
+                food_target.transform.SetParent(holopal.heldObjectTransform, false);
+                food_target.transform.localPosition = Vector3.zero;
+                food_target.GetComponent<Rigidbody>().isKinematic = true;
+                food_target.GetComponent<Rigidbody>().detectCollisions = false;
+                holopal.Nav_Agent.isStopped = true;
+                holopal.Nav_Agent.velocity = Vector3.zero;
+                holopal.animator.SetTrigger("eating");
+                eating = true;
             }
         }
     }
