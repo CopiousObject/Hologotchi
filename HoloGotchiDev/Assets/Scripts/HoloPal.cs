9a0a44a6914609b9b1670d72f5cabbbb5ba25fb2
Coruscat <j0nnyawes0me45@gmail.com>
2025-08-04 15:23:47 -0400

    Fix to Zell's import
    Minor adjustments to get it to work

--- HoloGotchiDev/Assets/Scripts/HoloPal.cs
+++ HoloGotchiDev/Assets/Scripts/HoloPal.cs
@@ -182,8 +182,8 @@ public class HoloPal : MonoBehaviour
         chatBubble.alpha = 0f;
 
         notifactions = true;
-        notiAudio = true;
-        notiVisual = true;
+        notiAudio = false;
+        notiVisual = false;
 
         next_act_out_time = DateTime.Now;
         overlay_image.enabled = false; // UI image with no sprite is a giant white square so disabled by default
